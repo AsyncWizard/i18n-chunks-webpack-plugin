590c6ddd51a4f87eadab4d40a8842506ab9d930d
AsyncWizard <no@mail.com>
2018-03-01 04:42:19 +0100

    Plugin index file with first base code.

---
+++ index.js
@@ -0,0 +1,149 @@
+/**
+ * Notes rapides
+ * Eviter d'utiliser les chemins relatifs. Préférer les arobases avec un resolve dans la conf (webpack.base.conf)
+ *    ou opter pour les identifiants de modules
+ */
+const path = require('path');
+const fs = require('fs');
+const SingleEntryPlugin = require('webpack/lib/SingleEntryPlugin');
+
+/**
+ * really really simple file sanitizer
+ * @param {string} file
+ * @return {string}
+ */
+function sanitize(file) {
+  return file.replace(/[/\\]/g, '_');
+}
+
+
+module.exports = class {
+  constructor(opts = {}) {
+    this.modules = opts.modules || [];
+    this.entriesPath = opts.distPath || path.join(process.cwd(), './webpack-i18n-entries');
+    if (!fs.existsSync(this.entriesPath)) { fs.mkdirSync(this.entriesPath); }
+  }
+  makeTmpSourcesFiles(i18nChunks) {
+    for (const [fileName, chunksList] of Object.entries(i18nChunks)) {
+      // Generating modules here
+
+      // TODO: generic i18nLib after ?
+      let moduleContent = `webpackI18NChunks.add('${chunksList.langId}'`;
+      for (const chunkPath of chunksList) { moduleContent += `, require('${chunkPath}')`; }
+      moduleContent += ')';
+
+      fs.writeFileSync(path.join(this.entriesPath, `${sanitize(fileName)}.js`), moduleContent);
+    }
+  }
+  startChildCompilation(compilation, filename) {
+    return new Promise((done, failed) => {
+      const outputOptions = {
+        filename,
+        publicPath: compilation.outputOptions.publicPath,
+      };
+      const childCompiler = compilation.createChildCompiler(`i18n-compiler${sanitize(filename)}`, outputOptions);
+      childCompiler.context = compilation.compiler.context;
+      childCompiler.apply(
+        new SingleEntryPlugin(
+          compilation.compiler.context,
+          path.join(this.entriesPath, sanitize(filename)),
+        ),
+      // new LoaderTargetPlugin('web')
+      );
+
+      childCompiler.runAsChild((err, entries, childCompilation) => {
+        // console.log('ChildCompilerDone', entries[0] ? entries[0].files : 'NO FILE ?!!!');
+        if (err) { return failed(err, entries, childCompilation); }
+        done(entries, childCompilation);
+      });
+    });
+  }
+  apply(compiler) {
+    // console.log('start plugin');
+    const { modules } = this;
+    /* compiler.plugin('emit', function(compilation, callback){
+     console.log('context', this.context);
+     callback();
+     });*/
+    compiler.plugin('emit', (compilation, callback) => {
+      // console.log('emit');
+
+      // Getting output directory. We didn't keep the first ./
+      const outputDirname
+      = `${path.dirname(compilation.outputOptions.chunkFilename)}/`
+        .replace(/^\.?\//, '');
+
+
+      /** Parcours des components chargés et génération d'un squelette de résultat
+     *
+     * Il devrait ressembler à:
+     * {
+       *   "chunkId-fr-fr": {
+       *     "path/to/i18n/fr-fr.ext": true
+       *   }
+       * }
+     *
+     */
+      const i18nChunks = {};
+      const { chunkFilename } = compilation.outputOptions;
+      for (const chunk of compilation.chunks) {
+        const chunkId = chunkFilename
+          .replace(/\.js$/i, '')
+          .replace(/\[id]/g, chunk.id)
+          .replace(/\[chunkhash]/g, chunk.renderedHash)
+      ;
+
+      // let i18nChunksFiles = [];
+        for (const normalModule of chunk.modules) {
+          for (const { rule, languagesFiles } of modules) {
+            if (rule.test(normalModule.portableId)) {
+            // On ajoute les fichiers de langue au chunk donné:
+
+            // Parcour des langues pour les ajouter:
+              for (const [langId, langPath] of Object.entries(languagesFiles)) {
+                const chunkI18NIdentifier = chunk.entryModule ? `${outputDirname + (chunk.name || 'entry')}-${langId}` : `${chunkId}-${langId}`;
+
+                if (typeof i18nChunks[chunkI18NIdentifier] === 'undefined') {
+                  const arr = [];
+                  arr.chunkId = chunkId;
+                  arr.langId = langId;
+                  i18nChunks[chunkI18NIdentifier] = arr;
+                }
+                /* eslint no-bitwise: ["error", { "allow": ["~"] }] */
+                if (!~i18nChunks[chunkI18NIdentifier].indexOf(langPath)) { i18nChunks[chunkI18NIdentifier].push(langPath); }
+              }
+            }
+          }
+        }
+      }
+      this.makeTmpSourcesFiles(i18nChunks);
+      Promise.all(
+        Object
+          .keys(i18nChunks)
+          .map(filename => this.startChildCompilation(compilation, `${filename}.js`)),
+      )
+        .then((entries) => {
+          const manifest = {
+            path: outputDirname,
+            filesList: entries
+              .reduce((acc, arr) => acc.concat(arr), [])
+              .map(chunk => chunk.files)
+              .reduce((acc, arr) => acc.concat(arr), []),
+          };
+
+          // TODO: Par la suite, ajouter tout autre info importantes dans ce manifest
+          const source = `webpackI18NManifest(${JSON.stringify(manifest)});`;
+          const { assets } = compilation;
+          assets['webpack-i18n-manifest.js'] = {
+            source() {
+              return source;
+            },
+            size() {
+              return source.length;
+            },
+          };
+          callback();
+        });
+    });
+  }
+};
